2899c5ada3db7b9bb633305857410b75993e4f47
Pedro Gabriel Barruetavena Vieira <107200364+pbarruetavena@users.noreply.github.com>
2024-04-14 17:38:47 -0300

    cheio de conflito esse usuario

--- tcc/src/main/java/com/mycompany/tcc/dados/Usuario.java
+++ tcc/src/main/java/com/mycompany/tcc/dados/Usuario.java
@@ -11,55 +11,19 @@ public class Usuario {
     private String bio;
     private int pontuacao;
     
-    private String pontuacao;
-    public Usuario(String nome, String bio, String pontuacao) {
+    public Usuario(String nome, String bio, int pontuacao) {
         this.nome = nome;
         this.bio = bio;
         this.pontuacao = pontuacao;
     }
 
-    public Usuario(String id, String nome, String bio, String pontuacao) {
+    public Usuario(String id, String nome, String bio, int pontuacao) {
         this.id = id;
         this.nome = nome;
         this.bio = bio;
         this.pontuacao = pontuacao;
     }
 
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getBio() {
-        return bio;
-    }
-
-    public void setBio(String bio) {
-        this.bio = bio;
-    }
-
-    public String getPontuacao() {
-        return pontuacao;
-    }
-
-    public void setPontuacao(String pontuacao) {
-        this.pontuacao = pontuacao;
-    }
-  
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id_user")
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
 
     public String getNome() {
         return nome;
@@ -83,6 +47,17 @@ public class Usuario {
 
     public void setPontuacao(int pontuacao) {
         this.pontuacao = pontuacao;
+    }
+  
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_user")
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
     }
   
 }
